87e403c7190e573f785c9d7902b12ea4dd8616ce
shoyuf <fanxiangyu2014@gmail.com>
2018-02-05 17:02:34 +0800

    fix bug

--- src/index.js
+++ src/index.js
@@ -7,12 +7,18 @@ Object.defineProperty(exports, "__esModule", {
 exports.default = function (content, config, file) {
   return new Promise(function (resolve, reject) {
     (0, _postcss2.default)([(0, _postcssCssnext2.default)({
-      cssnextConf: cssnextConf
+      features: {
+        customProperties: {
+          variables: {
+            mainColor: '#ff6d66'
+          }
+        }
+      }
     })]).process(content, {
       from: undefined
     }).then(function (res) {
       resolve(res.css);
-    }).catct(reject);
+    }).catch(reject);
   });
 };
 
@@ -27,13 +33,3 @@ var _postcssCssnext2 = _interopRequireDefault(_postcssCssnext);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 // import path from 'path'
-
-var cssnextConf = {
-  features: {
-    customProperties: {
-      variables: {
-        mainColor: '#ff6d66'
-      }
-    }
-  }
-};
